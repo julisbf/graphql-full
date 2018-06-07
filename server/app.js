14911c01f4249ea25779c898033213843e4cb6fc
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-06-07 18:01:42 -0400

    Add mongoose and dotenv functionality

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const app = express();
 const PORT = 4000;
+
+//Use for manage env variables
+require('dotenv').config();
+
+//Set the connection with DB
+mongoose.connect(`mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASS}@ds151970.mlab.com:51970/graphql-full`);
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB');
+})
 
 //Set up middleware endpoint to understand GraphQL
 app.use('/graphql', graphqlHTTP({
@@ -11,6 +21,7 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
+//Set the connection with server
 app.listen(PORT, () => {
   console.log(`Now listening for requests on PORT ${PORT}`);
 })
