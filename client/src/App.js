c797115c6c8eb7f967291917cd45bee8df70b1d4
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-06-11 14:34:51 -0400

    Initial Apollo Client set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import RecordList from './components/RecordList'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+//Apollo Client Setup
+const client = new ApolloClient({
+  uri: 'http:localhost:4000/graphql'
+})
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1> Records Inventory </h1>
-        <RecordList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1> Records Inventory </h1>
+          <RecordList />
+        </div>
+      </ApolloProvider>
     )
   }
 }
