c6a5500678065aafbbe68b6ee7f5fbb09a8318f0
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-06-11 14:24:58 -0400

    Create RecordList component

---
+++ client/src/components/RecordList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class RecordList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="record-list">
+          <li>Record Name</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default RecordList
